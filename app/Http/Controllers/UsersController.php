9ecfac76bf0f4a464cb77424927d719511959e96
Andres <tetesvillamil@gmail.com>
2021-03-19 13:52:39 -0500

    Añadir comments a UsersController

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -8,22 +8,24 @@ use Illuminate\Support\Facades\Http;
 class UsersController extends Controller
 {
     public function index(){
+        //GET request al API para obtener todos los datos
         $users = Http::get('https://jsonplaceholder.typicode.com/todos')->json();
         $collection = collect($users);
         $uniqueUserId = $collection->unique('userId');
-        //dd($uniqueUserId);
 
+        //Crando un array para contenter la cantidad de ToDo´s compledatos por cada usuario
         $completedArray = [];
         for($i = 1; $i <= count($uniqueUserId); $i++){
             $completedArray[$i] = 0;
         }
 
+        //Llenando array
         foreach($collection as $item){
             if ($item['completed'] == true) {
                 $completedArray[$item['userId']] += 1;
             }
         }
-        //dd($completedArray);
+        
         return view('users.index', compact('uniqueUserId', 'completedArray'));
     }
 }
